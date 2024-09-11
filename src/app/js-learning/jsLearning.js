29ab6b1ef85b187a05c161d9c064293fdedb5b7b
Rubizhanskyi <vitalii.rubizhanskyi@flixbus.com>
2024-09-11 14:48:03 +0200

    init commit

--- src/app/js-learning/jsLearning.js
+++ src/app/js-learning/jsLearning.js
@@ -1,3 +1,10 @@
-import {Person} from "./person.mjs";
-const value = (firstName, lastName, age) => new person.Person(firstName, lastName, age).sayHi();
-console.log(value('John', 'Doe', 34));
+import {Person} from "./person.js";
+
+const value = (firstName, lastName, age) => {
+  var person = new Person(firstName, lastName, age);
+  return person.sayHi();
+};
+
+console.log(value(process.env.FIRST, process.env.LAST, 14,));
+
+
--- src/app/js-learning/person.js
+++ src/app/js-learning/person.js
@@ -3,9 +3,30 @@ export class Person {
     this.name = name;
     this.lastName = lastName;
     this.age = age;
+    this.type = this.getType(age)
   }
 
   sayHi () {
-    return `I am ${this.name} ${this.lastName} - ${this.age} years old`
+    return `I am ${this.name} ${this.lastName} - ${this.age} years old, so I'm ${this.type}`
+  }
+  getType(age) {
+    var type = '';
+    switch (age) {
+      case 0: case 1:
+       type = 'Baby';
+       break;
+      case 2: case 3:
+        type = 'Toddler';
+        break;
+      case 4: case 5: case 6: case 7: case 8: case 9: case 10: case 11: case 12:
+        type = 'Child';
+        break;
+      case 13: case 14: case 15: case 16: case 17: case 18: case 19:
+        type = 'Teen';
+        break;
+      default:
+        type = 'Adult'
+    }
+    return type;
   }
 }
